bdc7d909b3612cd5d0b9c207a94c820089045d1b
Hamil Shah <hpshah@umich.edu>
2020-12-13 18:40:34 -0500

    resetting to prev move settings for real

--- state_machine.py
+++ state_machine.py
@@ -327,8 +327,8 @@ class StateMachine():
                     self.rxarm.disable_torque()
                     break
 
-                # self.rxarm.set_positions(joint_positions)
-                self.rxarm.set_joint_positions(joint_positions, moving_time=1.0, accel_time=0.05, blocking=True)
+                self.rxarm.set_positions(joint_positions)
+                # self.rxarm.set_joint_positions(joint_positions, moving_time=1.0, accel_time=0.05, blocking=True)
                 # check if we have changed gripper state
                 if prev_gripper_open != gripper_open:
                     # change gripper state to new state
